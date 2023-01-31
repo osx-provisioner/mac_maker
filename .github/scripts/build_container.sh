5f40433e0b6da46ce625edba0dbfcb4942f1a5ef
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-01-31 15:24:01 -0500

    build(DOCKER): flexible and enforced user perms

--- .github/scripts/build_container.sh
+++ .github/scripts/build_container.sh
@@ -4,7 +4,10 @@ set -eo pipefail
 
 main() {
 
-  docker-compose build --build-arg PYTHON_VERSION="${PYTHON_VERSION}"
+  docker-compose build                                        \
+    --build-arg BUILD_ARG_PYTHON_VERSION="${PYTHON_VERSION}"  \
+    --build-arg BUILD_ARG_CONTAINER_GID="$(id -g)"            \
+    --build-arg BUILD_ARG_CONTAINER_UID="$(id -u)"
   docker-compose up -d
 
 }
